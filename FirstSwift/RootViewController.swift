97b155d5e58afdde0987b8c21b5694c63c1e0f08
byobyohae <yoon.lee.uci@gmail.com>
2014-06-02 23:41:13 -0700

    initial commit

---
+++ FirstSwift/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  FirstSwift
+//
+//  Created by Yoon Lee on 6/2/14.
+//  Copyright (c) 2014 Yoon Lee. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+    }
+}
